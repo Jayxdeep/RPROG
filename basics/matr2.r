be22d00d172fe1aa7e6e6b226377caba26152c69
Jayxdeep <jaydeepmukherjee2003@gmail.com>
2025-03-27 00:07:27 +0530

    learned

---
+++ basics/matr2.r
@@ -0,0 +1,13 @@
+#2d matrices
+mat1 <- matrix(1:4,nrow=2)
+mat2 <- matrix(5:8,nrow=2)
+#element wise addition
+mat_sum <- mat1+mat2
+print(mat1)
+print(mat2)
+print(mat_sum)
+#matrix multiplication
+mat_mult <- mat1 * mat2
+print(mat1)
+print(mat2)
+print(mat_mult)
